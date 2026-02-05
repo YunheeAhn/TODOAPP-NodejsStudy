72ebe8d56e394c9d243aebac32da9ab0f1f40f8d
Yunning <ayh95944674@gmail.com>
2026-02-05 22:13:58 +0900

    feat : 벡엔드 배포시 포트 변경

--- TODOAPP-BE/app.js
+++ TODOAPP-BE/app.js
@@ -46,6 +46,11 @@ mongoose
   });
 
 //*** app listener 설정 ***//
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+// app.listen(5000, () => {
+//   console.log("Server is running on port 5000");
+// });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
