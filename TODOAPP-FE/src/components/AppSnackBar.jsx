6269bfdd994bff380f5dff28f50bdb6a18450069
Yunning <ayh95944674@gmail.com>
2026-02-05 19:36:19 +0900

    feat : 스낵바 컴포넌트 추가

---
+++ TODOAPP-FE/src/components/AppSnackBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import Fade from "@mui/material/Fade";
+
+const AppSnackbar = ({
+  open,
+  message,
+  severity = "success", // "success" | "error" | "info" | "warning"
+  onClose,
+  duration = 1200,
+  anchorOrigin = { vertical: "top", horizontal: "right" },
+}) => {
+  return (
+    <Snackbar
+      open={open}
+      onClose={onClose}
+      autoHideDuration={duration}
+      anchorOrigin={anchorOrigin}
+      TransitionComponent={Fade}
+    >
+      <Alert onClose={onClose} severity={severity} variant="filled" sx={{ alignItems: "center" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default AppSnackbar;
