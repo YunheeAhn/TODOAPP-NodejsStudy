b33fd6a45710dd9b1b79a189e1857a814c90c57a
Yunning <ayh95944674@gmail.com>
2026-02-08 17:45:43 +0900

    fix : 라우터 재설정

--- TODOAPP-FE/src/App.jsx
+++ TODOAPP-FE/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import TodoPage from "./pages/TodoPage/TodoPage";
--- TODOAPP-FE/src/main.jsx
+++ TODOAPP-FE/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 // mui
 import { ThemeProvider } from "@mui/material/styles";
@@ -10,9 +11,11 @@ import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
